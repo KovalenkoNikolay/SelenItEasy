d505ea600fd8b7605f35baa723d22b2589db1d4e
niko <kovalenko.mykola@gmail.com>
2017-09-29 22:04:05 +0300

    Add WebElements

---
+++ SelenItEasy/SelenItEasy.Core/WebElements/Button.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelenItEasy.Core.WebElements
+{
+    public class Button : WebElement
+    {
+        
+    }
+}
---
+++ SelenItEasy/SelenItEasy.Core/WebElements/WebElement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelenItEasy.Core.WebElements
+{
+    public abstract class WebElement
+    {
+
+    }
+}
