621b591aded3d66158dd1baaefa637b5823bccfe
KovalenkoNikolay <kovalenko.mykola@gmail.com>
2018-02-28 23:07:00 +0200

    Remove NInject dependency

--- SelenItEasy/SelenItEasy/SelenItEasy.Core/WebElements/Button.cs
+++ SelenItEasy/SelenItEasy/SelenItEasy.Core/WebElements/Button.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Ninject;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
